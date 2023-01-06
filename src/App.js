0a036c72dde23720606bc5a05c62b41a94ab446b
Theodore Bailey <tawbailey@hotmail.com>
2023-01-06 21:34:21 +0000

    Commit changes before bed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+// GIT ERROR
+// https://stackoverflow.com/questions/4181861/message-src-refspec-master-does-not-match-any-when-pushing-commits-in-git 
+// git push origin master 
 import React from 'react';
 // will look witin components header
 import { Header, Footer } from './components/common'
